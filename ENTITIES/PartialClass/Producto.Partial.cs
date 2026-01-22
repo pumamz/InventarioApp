985451dcb4598d843c62f76bbb366d12134892fc
pumamz <dpumamunoz@gmail.com>
2026-01-22 00:19:56 -0500

    Agregar archivos de proyecto.

---
+++ ENTITIES/PartialClass/Producto.Partial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES
+{
+    public partial class Producto
+    {
+        public string descripcionCategoria { get { return this.categoria.nombre; } }
+    }
+}
---
+++ SERVICES/APIService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using ENTITIES;
+namespace SERVICES
+{
+    public class APIService
+    {
+        private readonly HttpClient _httpClient;
+
+        public APIService() {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:3000/api/");
+        }
+
+        public async Task<Producto> ObtenerProducto(int id) {
+            HttpResponseMessage response = await _httpClient.GetAsync($"productos/{id}");
+            response.EnsureSuccessStatusCode();
+            string json = await response.Content.ReadAsStringAsync();
+            Producto producto = JsonConvert.DeserializeObject<Producto>(json);
+            return producto;
+        }
+
+        public async Task<List<Producto>> getAllProduct() {
+            HttpResponseMessage response = await _httpClient.GetAsync($"productos");
+            response.EnsureSuccessStatusCode();
+            string json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Producto>>(json);
+        }
+    }
+}
